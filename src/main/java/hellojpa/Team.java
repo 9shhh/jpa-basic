0862aa1359231cee03346bce9e88746e2769692a
9shhh <uelj1234@naver.com>
2023-02-12 23:09:53 +0900

    양방향 연관관계 정의 및 테스트

--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -3,6 +3,9 @@ package hellojpa;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -12,6 +15,9 @@ public class Team {
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "team") // team 이라는 명칭으로 매핑되어 있다는 의미.
+    List<Member> members = new ArrayList<>(); // 관례로 초기화 해준다. -> NPE 방지.
 
     public Long getId() {
         return id;
@@ -27,5 +33,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
